512f65145ee6a699d2ddf7776ad8f4abdaa655ce
tko-t <tkot.bom@gmail.com>
2024-06-06 02:57:42 +0900

    お化粧直し

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -9,6 +9,7 @@ export const App = () => {
   const [characterList, setCharacterList] = useState([]);
   const [mySelf, setMySelf] = useState(null);
   const [enemy, setEnemy]   = useState(null);
+  const [burnOut, setBurnOut] = useState(0);
 
   // csvロード
   useEffect(() => {
@@ -28,20 +29,18 @@ export const App = () => {
 
   // "発生", "ダメージ", "ガード硬直" があって
   // 事前動作の不要な攻撃リスト
-  // 自分用
   const frameTableForSelf = {};
   const frameTableBySelf = () => {
     if (!frameTableForSelf[mySelf]) {
-      const reg = /.*(中に|後に).*/
+      const reg = /.*(中に|後に|時に|段目).*/
       frameTableForSelf[mySelf] = sf6FrameData.filter((row) => {
-        return row.char == mySelf && row.fire && 0 < row.damage && row.guard && !reg.test(row.command)
+        return row.char == mySelf && row.fire && 0 < row.damage && row.guard && !reg.test(row.command) && !reg.test(row.name)
       })
     }
     return frameTableForSelf[mySelf];
   };
 
   // "ガード硬直" がある攻撃リスト
-  // 敵用
   const frameTableForEnemy = {};
   const frameTableByEnemy = () => {
     if (!frameTableForEnemy[enemy]) {
@@ -56,19 +55,17 @@ export const App = () => {
   useEffect(() => {
     if (!enemy) return;
 
-    frameTableBySelf()
     match()
 
-  }, [mySelf]);
+  }, [mySelf, burnOut]);
 
   // 敵のキャラ選択
   useEffect(() => {
     if (!mySelf) return;
 
-    frameTableByEnemy()
     match()
 
-  }, [enemy]);
+  }, [enemy, burnOut]);
 
   // 敵の行動のガード硬直より少ないフレームで発生できる技をフィルタ
   // {
@@ -86,16 +83,23 @@ export const App = () => {
     setMatchingTable(frameTableByEnemy().map(enemyRow => {
       const counter = {}
       frameTableBySelf().map(selfRow => {
-        if(-enemyRow.guard >= selfRow.fire) {
-          counter[enemyRow.name] ||= {
-            counters: [], eGuard: enemyRow.guard
+        // バーンアウト中は殴られ硬直が4フレーム長くなる
+        const eGuard   = parseInt(enemyRow.guard) + burnOut;
+        const eCommand = enemyRow.command;
+        const fire     = parseInt(selfRow.fire);
+        const damage   = selfRow.damage;
+        const command  = selfRow.command;
+        const counterKey = `${enemyRow.name} ： ${eCommand}`;
+        if(-eGuard >= fire) {
+          counter[counterKey] ||= {
+            counters: [], eGuard
           }
 
-          counter[enemyRow.name]['counters'].push({
+          counter[counterKey]['counters'].push({
             sName: selfRow.name,
-            sFire: selfRow.fire,
-            sDamage: selfRow.damage,
-            sCommand: selfRow.command,
+            sFire: fire,
+            sDamage: damage,
+            sCommand: command,
           })
         }
       })
@@ -110,9 +114,27 @@ export const App = () => {
 
     return (
       <div style={ { textAlign: 'center', width: "100%" } }>
-        <span style={ { fontWeight: 'bold'} }>ガード後に殴り返せるかもしれないリスト</span>
+        <details className="introduction">
+          <summary>
+            <span>ガード後に殴り返せるかもしれないリスト</span>
+          </summary>
+          <div className="detail_item coutions">
+            <small>敵の不利フレームより速い発生を持つ技の一覧です</small>
+            <small>ガード後の距離やリーチは考慮してないです</small>
+            <small>[...]は不利フレーム</small>
+            <small>202405 Ver.</small>
+          </div>
+        </details>
       </div>
     )
+  }
+
+  const onBurnout = (e) => {
+    if (e.target.checked == true) {
+      setBurnOut(4); // バーンアウト中は殴られ硬直が4フレーム長くなる
+    } else {
+      setBurnOut(0);
+    }
   }
 
   return (
@@ -126,6 +148,10 @@ export const App = () => {
           <CharaSelecter placeholder="敵" list={characterList} onChange={setEnemy}/>
         </div>
       </HBox>
+      <div style={ { marginLeft: "10px", padding: "4px 8px 8px 0"} }>
+        <input id="burnout" type="checkbox" onChange={ (e) => onBurnout(e) }/>
+        <label htmlFor="burnout">バーンアウト中</label>
+      </div>
       { summaryHeader() }
       <div>
         { matchingTable.map((matching, idx) => {
@@ -133,21 +159,31 @@ export const App = () => {
           const targetArtsFrame = Object.values(matching)[0].eGuard
           const counters = Object.values(matching)[0].counters
           return (
-            <details key={idx}>
-              <summary>{`${targetArts}(${targetArtsFrame})`}</summary>
-              <div>
-              { counters.map(counter => {
+            <details key={targetArts}>
+              <summary>
+                <span style={ { minWidth: "40px", display: "inline-block" } }>[{targetArtsFrame}]</span>
+                <span>{targetArts}</span>
+              </summary>
+              { counters.map((counter, num) => {
                 const counterName = counter.sName;
                 const counterFireFrame = counter.sFire;
-                const counterCommand = counter.command;
+                const counterCommand = counter.sCommand;
                 return (
-                  <div key={Math.random()}>
-                    <p>{`${counterName}[${counterFireFrame}]`}</p>
-                    <p>{counterCommand}</p>
+                  <div key={Math.random()}  className={`detail_item counter_${ num % 2 == 0 ? "a" : "b"}`}>
+                    <div>
+                      <span>{ counterName}</span>
+                    </div>
+                    <div>
+                      <span>発生：</span>
+                      <span>{ counterFireFrame }</span>
+                    </div>
+                    <div>
+                      <span>コマンド：</span>
+                      <span>{counterCommand}</span>
+                    </div>
                   </div>
                 )
               })}
-              </div>
             </details>
           )
         })}
